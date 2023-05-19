3e13bbea534383f20aa991bb5ccf485f60777648
Dyhless <108591328+Dyhless@users.noreply.github.com>
2023-05-19 17:19:25 +0000

    Deploying to gh-pages from @ Dyhless/goit-js-hw-09@80343c66fb50eacb82e13a5f157bcfef64fee2ef 🚀

---
+++ 01-color-switcher.51ef5a6a.js
@@ -0,0 +1,2 @@
+!function(){var t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]")},n=null,o=!1;t.startBtn.addEventListener("click",(function(){o||(n=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3),o=!0,t.startBtn.disabled=!0)})),t.stopBtn.addEventListener("click",(function(){clearInterval(n),document.body.style.backgroundColor="",o=!1,t.startBtn.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.51ef5a6a.js.map
