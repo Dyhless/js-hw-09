80343c66fb50eacb82e13a5f157bcfef64fee2ef
Dmytro Medvediev <dsisitkin@gmail.com>
2023-05-19 19:19:00 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
-//
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+const refs = {
+   startBtn: document.querySelector('button[data-start]'),
+   stopBtn: document.querySelector('button[data-stop]'),
+};
+
+let intervalId = null;
+let isIntervalActive = false;
+
+refs.startBtn.addEventListener('click', () => {
+   if (isIntervalActive) {
+      return;
+   }
+   intervalId = setInterval(() => {
+      document.body.style.backgroundColor = getRandomHexColor();
+   }, 1000);
+   isIntervalActive = true;
+   refs.startBtn.disabled = true;
+});
+
+refs.stopBtn.addEventListener('click', () => {
+   clearInterval(intervalId);
+   document.body.style.backgroundColor = '';
+   isIntervalActive = false;
+   refs.startBtn.disabled = false;
+});
