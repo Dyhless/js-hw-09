c45c6bb3228a07a428c0af1def1a9b77f62abebe
Dmytro Medvediev <dsisitkin@gmail.com>
2023-05-22 18:35:32 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,30 +1,48 @@
-import Notiflix from "notiflix";
+import Notiflix from 'notiflix';
 
-const refs = {
-  form: document.querySelector("[form]"),
-  delay: document.querySelector("[delay]"),
-  step: document.querySelector("[step]"),
-  amount: document.querySelector("[amount]"),
-  submit: document.querySelector("submit[submit]"),
-};
+// Находим форму по классу и добавляем обработчик события submit
+const form = document.querySelector('.form');
+form.addEventListener('submit', handleSubmit);
 
-const { form, delay, step, amount, submit } = refs;
+// Обработчик события submit формы
+function handleSubmit(event) {
+  event.preventDefault();
 
-const shouldResolve = true;
+  // Получаем значения полей ввода и преобразуем их в числа
+  const delay = Number(document.querySelector('input[name="delay"]').value);
+  const step = Number(document.querySelector('input[name="step"]').value);
+  const amount = Number(document.querySelector('input[name="amount"]').value);
 
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  // Создаем промисы с заданными параметрами
+  createPromises(amount, delay, step);
 }
 
-createPromise(2, 1500)
-  .then(({ position, delay }) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-  });
+// Создание промисов
+function createPromises(amount, delay, step) {
+  for (let i = 1; i <= amount; i++) {
+    const position = i;
+    const currentDelay = delay + (i - 1) * step;
+
+    // Создаем промис с задержкой
+    const promise = new Promise((resolve, reject) => {
+      const shouldResolve = Math.random() > 0.3;
+
+      setTimeout(() => {
+        if (shouldResolve) {
+          resolve({ position, delay: currentDelay });
+        } else {
+          reject({ position, delay: currentDelay });
+        }
+      }, currentDelay);
+    });
+
+    // Обрабатываем результат промиса
+    promise
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+}
