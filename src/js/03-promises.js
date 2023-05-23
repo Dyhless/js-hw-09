fc5c0071b60c292a898e67dd6e0977560d331ef4
Dmytro Medvediev <dsisitkin@gmail.com>
2023-05-23 21:10:31 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,24 +1,18 @@
 import Notiflix from 'notiflix';
 
-
 const form = document.querySelector('.form');
 form.addEventListener('submit', handleSubmit);
 
-
 function handleSubmit(event) {
   event.preventDefault();
-
 
   const delay = Number(document.querySelector('input[name="delay"]').value);
   const step = Number(document.querySelector('input[name="step"]').value);
   const amount = Number(document.querySelector('input[name="amount"]').value);
 
-  if (handleSubmit) {
-    form.reset();
-  }
+  form.reset();
 
-
-  createPromises(position, delay)
+  createPromises(0, delay)
     .then(({ position, delay }) => {
       Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
     })
@@ -27,19 +21,16 @@ function handleSubmit(event) {
     });
 }
 
-
 function createPromises(position, delay) {
-
   return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
 
     setTimeout(() => {
       if (shouldResolve) {
-        resolve({ position, delay});
+        resolve({ position, delay });
       } else {
-        reject({ position, delay});
+        reject({ position, delay });
       }
     }, delay);
   });
 }
-
