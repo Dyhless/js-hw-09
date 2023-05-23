467775459664cdc42f82675b41b5fbe35cfa8cc7
Dmytro Medvediev <dsisitkin@gmail.com>
2023-05-23 20:51:44 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,19 +1,23 @@
 import Notiflix from 'notiflix';
 
-// Находим форму по классу и добавляем обработчик события submit
+
 const form = document.querySelector('.form');
 form.addEventListener('submit', handleSubmit);
 
-// Обработчик события submit формы
+
 function handleSubmit(event) {
   event.preventDefault();
 
-  // Получаем значения полей ввода и преобразуем их в числа
+
   const delay = Number(document.querySelector('input[name="delay"]').value);
   const step = Number(document.querySelector('input[name="step"]').value);
   const amount = Number(document.querySelector('input[name="amount"]').value);
 
-  // Создаем промисы с заданными параметрами
+  if (handleSubmit) {
+    form.reset();
+  }
+
+
   createPromises(position, delay)
     .then(({ position, delay }) => {
       Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
@@ -23,7 +27,7 @@ function handleSubmit(event) {
     });
 }
 
-// Создание промисов
+
 function createPromises(position, delay) {
 
   return new Promise((resolve, reject) => {
