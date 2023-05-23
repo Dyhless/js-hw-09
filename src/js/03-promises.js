ee7f3def9d917a2808b12527bbcb171f9c7428c9
Dmytro Medvediev <dsisitkin@gmail.com>
2023-05-23 20:32:43 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -14,36 +14,28 @@ function handleSubmit(event) {
   const amount = Number(document.querySelector('input[name="amount"]').value);
 
   // Создаем промисы с заданными параметрами
-  createPromises(amount, delay, step);
+  createPromises(position, delay)
+    .then(({ position, delay }) => {
+      Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
 }
 
 // Создание промисов
-function createPromises(amount, delay, step) {
-  for (let i = 1; i <= amount; i++) {
-    const position = i;
-    const currentDelay = delay + (i - 1) * step;
+function createPromises(position, delay) {
 
-    // Создаем промис с задержкой
-    const promise = new Promise((resolve, reject) => {
-      const shouldResolve = Math.random() > 0.3;
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
 
-      setTimeout(() => {
-        if (shouldResolve) {
-          resolve({ position, delay: currentDelay });
-        } else {
-          reject({ position, delay: currentDelay });
-        }
-      }, currentDelay);
-    });
-
-    
-    // Обрабатываем результат промиса
-    promise
-      .then(({ position, delay }) => {
-        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-      })
-      .catch(({ position, delay }) => {
-        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-      });
-  }
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay});
+      } else {
+        reject({ position, delay});
+      }
+    }, delay);
+  });
 }
+
